c7f7bf6cdd664d2cf1aa9b0180e5403f35e462e3
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-24 21:49:40 +0200

    Updates

---
+++ precache-manifest.9294c833517f07eb6ede1f13440316cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4f70b3cf3d7afd73f81dab36d9a58d",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "f2ee66585dc3d2b13632",
+    "url": "/react_tabs-with-router/static/css/main.be7079c9.chunk.css"
+  },
+  {
+    "revision": "8f2e89b5ffa20979cc72",
+    "url": "/react_tabs-with-router/static/js/2.d20019d1.chunk.js"
+  },
+  {
+    "revision": "f2ee66585dc3d2b13632",
+    "url": "/react_tabs-with-router/static/js/main.c6332c35.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
